e0993ba6fc064b15bebb7fc41db28ea5d85987aa
ilCriptoEconomista <ilcriptoeconomista@gmail.com>
2024-12-14 18:04:55 +0100

    Example input/output

---
+++ input_output.cxx
@@ -0,0 +1,19 @@
+#include  <iostream>
+using namespace std;
+
+int main()
+{
+string name;
+int age;
+
+cout <<"Hey! What's yohr name?";
+cin>>name;
+
+cout<<"What is your age?";
+cin>>age;
+
+cout<<"Hey " << name << " you are " << age << " years old "<<
+endl;
+
+return 0;
+}
