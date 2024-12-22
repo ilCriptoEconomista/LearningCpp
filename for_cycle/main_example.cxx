0d071706c99a54aa6622886b17f50148f23e21ba
ilCriptoEconomista <ilcriptoeconomista@gmail.com>
2024-12-22 22:45:18 +0100

    Add files via upload

---
+++ for_cycle/main_example.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	// for (inizializzazione; condizione; aggiornamento) {
+		// corpo del ciclo
+		// istruzioni da ripetere
+		//}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "i = " << i <<
+		endl;
+	}
+	return 0;
+}
